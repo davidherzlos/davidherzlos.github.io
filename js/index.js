4bf95b12d68588dd9c3d76da08626a9c896184fd
davidherzlos <davidherzlos@gmail.com>
2019-04-10 00:12:03 -0500

    feat(Porfolio): update projects with my own projects and fix some inconsistencies in styles.
    The projects section was outdated showing only examples, also
    the UI was incosistent with the styles.
    Now the section shows my own projects information and renders
    correctly.

--- js/index.js
+++ js/index.js
@@ -18,58 +18,76 @@
 
   // Projects section
   const projectsData = [
-    { title: 'Canon Training Platform', img: './static/project-image2.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'Podcasts WebApp', img: './static/project-image.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'UnAQ Virtual Platform', img: './static/project-image2.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'Formacion Integral UnADM', img: './static/project-image.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'RAD Repository UnADM', img: './static/project-image2.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'Collaborate Moodle Plugin', img: './static/project-image.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'Moodle Tunnig UnADM', img: './static/project-image2.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'Articulate Courses', img: './static/project-image.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'Ration WebApp', img: './static/project-image2.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'Laratter WebApp', img: './static/project-image.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'Canon Training Platform', img: './static/project-image2.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'Podcasts WebApp', img: './static/project-image.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'UnAQ Virtual Platform', img: './static/project-image2.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'Formacion Integral UnADM', img: './static/project-image.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'RAD Repository UnADM', img: './static/project-image2.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'Collaborate Moodle Plugin', img: './static/project-image.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'Moodle Tunnig UnADM', img: './static/project-image2.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'Articulate Courses', img: './static/project-image.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'Ration WebApp', img: './static/project-image2.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'Laratter WebApp', img: './static/project-image.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'Canon Training Platform', img: './static/project-image2.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'Podcasts WebApp', img: './static/project-image.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'UnAQ Virtual Platform', img: './static/project-image2.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'Formacion Integral UnADM', img: './static/project-image.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'RAD Repository UnADM', img: './static/project-image2.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'Collaborate Moodle Plugin', img: './static/project-image.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'Moodle Tunnig UnADM', img: './static/project-image2.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'Articulate Courses', img: './static/project-image.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'Ration WebApp', img: './static/project-image2.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'Laratter WebApp', img: './static/project-image.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'Canon Training Platform', img: './static/project-image2.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'Podcasts WebApp', img: './static/project-image.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'UnAQ Virtual Platform', img: './static/project-image2.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'Formacion Integral UnADM', img: './static/project-image.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'RAD Repository UnADM', img: './static/project-image2.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'Collaborate Moodle Plugin', img: './static/project-image.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'Moodle Tunnig UnADM', img: './static/project-image2.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'Articulate Courses', img: './static/project-image.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'Ration WebApp', img: './static/project-image2.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' },
-    { title: 'Laratter WebApp', img: './static/project-image.png', alt: 'Project alt text', info: 'Frontend Dev, Design', description: 'lorem ipsum bla bla bla lorem amend' }
+    {
+      'title': 'Formación Integral',
+      'description': `Sharing items with Ration is super easy. Take a picture of stuff you don't want to take up space in your home, but still kinda want to keep in case you need it someday. Then, if a friend needs it, they'll come get it!`,
+      'url': 'http://formacionintegral.unadmexico.mx/FI',
+      'cover': './static/fi.png',
+      'tags': ['PHP', 'CSS3', 'Javascript', 'Moodle', 'Linux']
+    },
+    {
+      'title': 'Ration IO App',
+      'description': `Sharing items with Ration is super easy. Take a picture of stuff you don't want to take up space in your home, but still kinda want to keep in case you need it someday. Then, if a friend needs it, they'll come get it!`,
+      'url': 'https://ration.io',
+      'cover': './static/ration.png',
+      'tags': ['Sails.js', 'APIS', 'Stripe', 'Mailgun', 'Bootstrap', 'EJS', 'Heroku']
+    },
+    {
+      'title': 'Podcasts App',
+      'description': `Sharing items with Ration is super easy. Take a picture of stuff you don't want to take up space in your home, but still kinda want to keep in case you need it someday. Then, if a friend needs it, they'll come get it!`,
+      'url': 'https://davidherzlos-podcasts-smbzjpwwtj.now.sh',
+      'cover': './static/podcasts.png',
+      'tags': ['APIS', 'React', 'Next.Js', 'CSS Grids', 'Now.sh']
+    },
+    {
+      'title': 'Repositorio Institucional UnADM',
+      'description': `Sharing items with Ration is super easy. Take a picture of stuff you don't want to take up space in your home, but still kinda want to keep in case you need it someday. Then, if a friend needs it, they'll come get it!`,
+      'url': 'http://www.repositorio.unadmexico.mx:8080',
+      'cover': './static/ri-unadm.png',
+      'tags': ['Html5', 'Dspace', 'Linux', 'CSS', 'PostgreSQL']
+    },
+    {
+      'title': 'Canon Demo Platform',
+      'description': `Sharing items with Ration is super easy. Take a picture of stuff you don't want to take up space in your home, but still kinda want to keep in case you need it someday. Then, if a friend needs it, they'll come get it!`,
+      'url': 'http://52.204.73.6',
+      'cover': './static/canon-demo.png',
+      'tags': ['PHP', 'CSS3', 'Javascript', 'Moodle', 'Linux']
+    },
+    {
+      'title': 'Unaq Virtual',
+      'description': `Sharing items with Ration is super easy. Take a picture of stuff you don't want to take up space in your home, but still kinda want to keep in case you need it someday. Then, if a friend needs it, they'll come get it!`,
+      'url': 'http://unaq.mrooms.net',
+      'cover': './static/unaq.png',
+      'tags': ['PHP', 'CSS3', 'Jquery', 'Linux']
+    },
+    {
+      'title': 'Expensy App',
+      'description': `Sharing items with Ration is super easy. Take a picture of stuff you don't want to take up space in your home, but still kinda want to keep in case you need it someday. Then, if a friend needs it, they'll come get it!`,
+      'url': 'http://expensy-davidherzlos.herokuapp.com',
+      'cover': './static/expensy.png',
+      'tags': ['PHP', 'Laravel', 'Bootstrap', 'Heroku']
+    },
+    {
+      'title': 'Page Speed Insights',
+      'description': `Sharing items with Ration is super easy. Take a picture of stuff you don't want to take up space in your home, but still kinda want to keep in case you need it someday. Then, if a friend needs it, they'll come get it!`,
+      'url': 'https://davidherzlos.github.io/page-speed-insights',
+      'cover': './static/page-speed.png',
+      'tags': ['HTML5', 'CSS', 'Javascript']
+    }
   ];
-    
+  
   const projectTemplate = function(project) {
     return (`
     <article class="project">
-      <a class="project_link-container" href="#">
+      <a class="project_link-container" href="${project.url}">
         <h1 class="project_title">${project.title}</h1>
         <figure class="project_image-wrapper">
-          <img class="project_image" src="${project.img}" alt="${project.alt}">
+          <img class="project_image" src="${project.cover}" alt="${project.title}">
           <figcaption class="project_info-wrapper">
-            <span>${project.info}</span>
             <p class="project_info">${project.description}</p>
+            <div class="skills">
+              ${project.tags.map(tag => '<span>' + tag + '</span>').join('')}
+            </div>
           </figcaption>
         </figure>
       </a>
