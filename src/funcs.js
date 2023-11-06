d3756c6137382ae299d4582312b7f73611f2afb2
David OC <davidherzlos@gmail.com>
2023-11-06 00:44:43 -0600

    refactor: re-organize code in es6 modules

---
+++ src/funcs.js
@@ -0,0 +1,40 @@
+const funcs = {};
+
+funcs.curried = {};
+
+funcs.curried.renderHTML = function(template) {
+    return function(list){
+        return list.map( object => template(object) );
+    };
+};
+
+funcs.curried.addToDOM = function (selector, position) {
+    return async (htmlChilds) => {
+        const $container = document.querySelector(selector);
+        htmlChilds.forEach(function (child) {
+            $container.insertAdjacentHTML(position, child);
+        });
+    };
+};
+
+funcs.curried.display = function (renderFunc, domFunc) {
+    return function(list){
+        domFunc(renderFunc(list));
+    };
+};
+
+funcs.createIterator = function*(data) {
+    for (const item of data) {
+        yield item
+    }
+};
+
+funcs.getFromIterator = function(amount, iteratorObject){
+    var items = []
+    for (let idx = 0; idx < amount; idx++) {
+        items.push(iteratorObject.next().value)
+    }
+    return items
+};
+
+export default funcs;
